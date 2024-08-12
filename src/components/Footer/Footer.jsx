ce16496e9f771aba141f25a4217d254dfc63cb11
javokhiryoldashov <yuldoshovjavohir3@gmail.com>
2024-08-12 12:32:48 +0500

    fenish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaGithub,} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -16,33 +16,13 @@ const Footer = () => {
 
         <div className="flex justify-center space-x-6 mt-2">
           <Link
-            to="https://github.com/MusayevDoniyor/"
+            to="https://github.com/javakhiryoldashov"
             className="text-gray-400 hover:text-red-500 transition-colors duration-200"
             aria-label="GitHub"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaGithub size={24} />
-          </Link>
-
-          <Link
-            to="https://www.linkedin.com/in/doniyor-musayev-2783592a4/"
-            className="text-gray-400 hover:text-red-500 transition-colors duration-200"
-            aria-label="LinkedIn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin size={24} />
-          </Link>
-
-          <Link
-            to="https://www.instagram.com/d0niyor.m/"
-            className="text-gray-400 hover:text-red-500 transition-colors duration-200"
-            aria-label="Instagram"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram size={24} />
           </Link>
         </div>
       </div>
